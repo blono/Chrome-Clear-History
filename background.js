029bebad92a4f42cb64536ba6b6dde40d750735c
author <author@example.com>
2022-05-16 20:00:18 +0900

    Add url search

--- background.js
+++ background.js
@@ -10,3 +10,38 @@ chrome.storage.sync.get('days', storage => {
     endTime: Date.now() - (days * 24 * 60 * 60 * 1000)
   });
 });
+
+chrome.storage.sync.get('search', storage => {
+  let search = storage.search;
+
+  if (search == null || typeof(search) !== 'string') {
+    return;
+  }
+
+  search = search.trim();
+  if (search == '') {
+    return;
+  }
+
+  search = search.split(',');
+  if (search.length == null || search.length <= 0) {
+    return;
+  }
+
+  search.forEach(subSearch => {
+    chrome.history.search({
+      startTime: 0,
+      endTime: Date.now(),
+      maxResults: 0,
+      text: subSearch.trim()
+    }).then(searchResult => {
+      searchResult.forEach(history => {
+        chrome.history.deleteUrl({
+          url: history.url
+        });
+      });
+    }).catch(error => {
+      console.error(error);
+    });
+  });
+});
--- options.js
+++ options.js
@@ -6,24 +6,62 @@ addEventListener('DOMContentLoaded', _ => {
       return;
     }
 
-    document.querySelector('#days').value = days;
+    document.getElementById('days').value = days;
+  });
+  chrome.storage.sync.get('search', storage => {
+    const search = storage.search;
+
+    if (search == null || typeof(search) !== 'string') {
+      return;
+    }
+
+    document.getElementById('search').value = search;
   });
 
-  document.querySelector('#days').addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      let days = 0;
+  const saveDays = value => {
+    let days = 9999;
 
-      try {
-        days = parseInt(this.value, 10);
-      } catch (e) {
-        console.error(e);
-
-        days = 0;
+    try {
+      days = Math.max(0, parseInt(value, 10));
+      if (isNaN(days) || !isFinite(days)) {
+        days = 9999;
       }
+    } catch (e) {
+      console.error(e);
 
+      days = 9999;
+    }
+
+    return new Promise(resolve => {
       chrome.storage.sync.set({
         days: days
       }, () => {
+        resolve();
+      });
+    });
+  }
+
+  const saveSearch = value => {
+    return new Promise(resolve => {
+      chrome.storage.sync.set({
+        search: (value == null || value.trim == null) ? '' : value.trim()
+      }, () => {
+        resolve();
+      });
+    });
+  }
+
+  document.getElementById('days').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      Promise.all([saveDays(this.value), saveSearch(document.getElementById('search').value)]).then(() => {
+        window.close();
+      });
+    }
+  });
+
+  document.getElementById('search').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      Promise.all([saveDays(document.getElementById('days').value), saveSearch(this.value)]).then(() => {
         window.close();
       });
     }
